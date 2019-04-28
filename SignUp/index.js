465530bbba973af00460111d0484eeb4cf2da66e
josephHai <junyan0803@gmail.com>
2019-04-28 23:03:41 +0800

    输入框信息自动填充并自动提交

--- SignUp/index.js
+++ SignUp/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 chrome.storage.sync.get(null, (v) => {
-  console.log(v);
+  const p = new Promise((resolve, reject) => {
+    document.getElementById('q1').innerText = v.name;
+    document.getElementById('q2').innerText = v.sid;
+    document.getElementById('q3').innerText = v.tel;
+    setTimeout(() => {
+      resolve('submit');
+    }, 1000);
+  });
+
+  p.then((v) => {
+    document.getElementById('submit_button').click();
+  });
 });
