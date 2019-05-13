2022515be881d1b50222113ad7fa641ace757215
josephHai <junyan0803@gmail.com>
2019-05-13 16:02:56 +0800

    根据表单的字段名来填写

--- SignUp/index.js
+++ SignUp/index.js
@@ -2,15 +2,37 @@
 
 chrome.storage.sync.get(null, (v) => {
   const p = new Promise((resolve, reject) => {
-    document.getElementById('q1').value = v.name;
-    document.getElementById('q2').value = v.sid;
-    document.getElementById('q3').value = v.tel;
+    const fieldMap = [
+      {field: "学号", value: v.sid},
+      {field: "姓名", value: v.name},
+      {field: "联系方式", value: v.tel}
+    ];
+
+    const el1 = document.getElementById('q1');
+    const el2 = document.getElementById('q2');
+    const el3 = document.getElementById('q3');
+    const text1 = el1.parentNode.previousSibling.innerText;
+    const text2 = el2.parentNode.previousSibling.innerText;
+    const text3 = el3.parentNode.previousSibling.innerText;
+
+    fieldMap.map((k) => {
+      if (!el1.value && text1.indexOf(k.field) !== -1) {
+        el1.value = k.value;
+      }
+      if (!el2.value && text2.indexOf(k.field) !== -1) {
+        el2.value = k.value;
+      }
+      if (!el3.value && text3.indexOf(k.field) !== -1) {
+        el3.value = k.value;
+      }
+    })
+
     setTimeout(() => {
       resolve('submit');
     }, 1000);
   });
 
   p.then((v) => {
-    document.getElementById('submit_button').click();
+    // document.getElementById('submit_button').click();
   });
 });
